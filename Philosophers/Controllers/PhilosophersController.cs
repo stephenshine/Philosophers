6d0019da07d7d3db694424e3a2d0dc75d5c3c6e0
Stephen Shine <stephen.shine88@gmail.com>
2016-11-02 20:31:43 +0000

    Adding area filter to index method

--- Philosophers/Controllers/PhilosophersController.cs
+++ Philosophers/Controllers/PhilosophersController.cs
@@ -15,8 +15,17 @@ namespace Philosophers.Controllers
         private PhilosopherDBContext db = new PhilosopherDBContext();
 
         // GET: Philosophers
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string area, string searchString)
         {
+            var AreaList = new List<string>();
+
+            var AreaQry = from p in db.Philosophers
+                          orderby p.Area
+                          select p.Area;
+
+            AreaList.AddRange(AreaQry.Distinct());
+            ViewBag.area = new SelectList(AreaList);
+
             var philosophers = from p in db.Philosophers
                                select p;
 
@@ -24,6 +33,11 @@ namespace Philosophers.Controllers
             {
                 philosophers = philosophers.Where(p => p.LastName.Contains(searchString));
             }
+            if (!String.IsNullOrEmpty(area))
+            {
+                philosophers = philosophers.Where(p => p.Area == area);
+            }
+
             return View(philosophers);
         }
 
