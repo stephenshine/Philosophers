25d024dfa9cd86a7b2553e35a51490d140ee833f
Stephen Shine <stephen.shine88@gmail.com>
2016-11-02 20:18:07 +0000

    adding data annotations to P model

--- Philosophers/Models/Philosopher.cs
+++ Philosophers/Models/Philosopher.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Philosophers.Models
 {
     public class Philosopher
     {
         public int PhilosopherID { get; set; }
+
+        [Display (Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Date of death")]
+        [DataType(DataType.Date)]
         public DateTime DateOfDeath { get; set; }
         public string Area { get; set; }
         public string Nationality { get; set; }
