db0fa30fc268d30a33a1682fff3e312a832645b2
Stephen Shine <stephen.shine88@gmail.com>
2016-11-02 20:02:59 +0000

    Installing EF and adding P model

---
+++ Philosophers/Models/Philosopher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Philosophers.Models
+{
+    public class Philosopher
+    {
+        public int PhilosopherID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }
+        public string Area { get; set; }
+        public string Nationality { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class PhilosopherDBContext : DbContext
+    {
+        public DbSet<Philosopher> Philosophers { get; set; }
+    }
+}
