d33ef751a386f210d61dc4107118656b6023a2ca
Stephen Shine <stephen.shine88@gmail.com>
2016-11-02 21:54:48 +0000

    Creating Area class

---
+++ Philosophers/Models/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Philosophers.Models
+{
+    public class Area
+    {
+        public int AreaId { get; set; }
+        public int Name { get; set; }
+
+        public virtual ICollection<Philosopher> Philosophers { get; set; }
+    }
+}
--- Philosophers/Models/Philosopher.cs
+++ Philosophers/Models/Philosopher.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
