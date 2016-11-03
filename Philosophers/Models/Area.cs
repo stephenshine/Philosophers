abaccb0193e25359fadafa43d2c09f456c023f0f
Stephen Shine <stephen.shine88@gmail.com>
2016-11-03 12:28:36 +0000

    Adding data annotations to Area model

--- Philosophers/Models/Area.cs
+++ Philosophers/Models/Area.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace Philosophers.Models
     public class Area
     {
         public int AreaId { get; set; }
+        
+        [Display(Name = "Area"}]
         public string Name { get; set; }
 
         public virtual ICollection<Philosopher> Philosophers { get; set; }
