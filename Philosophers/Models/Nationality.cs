2d2e8c5e9edd37de2a4b26bdc32a6be9f298b585
Stephen Shine <stephen.shine88@gmail.com>
2016-11-03 22:03:39 +0000

    Adding Nationality model

---
+++ Philosophers/Models/Nationality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Philosophers.Models
+{
+    public class Nationality
+    {
+        public int NationalityID { get; set; }
+
+        [Display(Name = "Nationality")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Philosopher> Phliosophers { get; set; }
+    }
+}
