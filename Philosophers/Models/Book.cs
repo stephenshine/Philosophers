651319ea4b3bd4c6b33305a1ca65719a9f40be0c
Stephen Shine <stephen.shine88@gmail.com>
2016-11-06 16:07:28 +0000

    Adding book model

---
+++ Philosophers/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Philosophers.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+
+        [StringLength(100)]
+        public string Title { get; set; }
+        public int PhilosopherID { get; set; }
+
+        public virtual Philosopher Philosopher { get; set; }
+    }
+}
