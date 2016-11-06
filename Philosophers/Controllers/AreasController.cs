0c0434d22fc08d6a671b6abcfdb5cc3b18bb3fda
Stephen Shine <stephen.shine88@gmail.com>
2016-11-06 08:34:44 +0000

    Adding Areas controller

---
+++ Philosophers/Controllers/AreasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Philosophers.Models;
+
+namespace Philosophers.Controllers
+{
+    public class AreasController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
