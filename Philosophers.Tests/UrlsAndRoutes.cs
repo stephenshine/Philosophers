2ff61d571f5a455b28487d50e1e3a886561f6b6b
Stephen Shine <stephen.shine88@gmail.com>
2016-11-07 09:38:53 +0000

    Adding testIncomingResult method to test file

--- Philosophers.Tests/UrlsAndRoutes.cs
+++ Philosophers.Tests/UrlsAndRoutes.cs
@@ -34,6 +34,45 @@ namespace Philosophers.Tests
             return mockContext.Object;
         }
 
+        private void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            // arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // act
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(v1, v2) == 0;
+            };
+
+            bool result = valCompare(routeResult.Values["controller"], controller) && valCompare(routeResult.Values["action"], action);
+
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name) && valCompare(routeResult.Values[pi.Name], pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
