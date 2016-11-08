7615014036f57aff1bea12b51ff6c328a4df848a
Stephen Shine <stephen.shine88@gmail.com>
2016-11-08 08:26:05 +0000

    Adding custom html helper class

---
+++ Philosophers/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,20 @@
+﻿using Philosophers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Philosophers.Infrastructure
+{
+    public static class CustomHelpers
+    {
+
+        public static MvcHtmlString PhilosopherData(this HtmlHelper html, Philosopher philosopher)
+        {
+
+
+            return new MvcHtmlString();
+        }
+    }
+}
